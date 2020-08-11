cc4efb5c5d02f69b8232822cb709465d3586a4e8
javiBarrientos <javibarrientosfernandez99@gmail.com>
2020-08-11 19:37:17 +0200

    feat: Completar main
    Añadidos los ultimos print

--- src/main/java/edu/elsmancs/loremipsum/Main.java
+++ src/main/java/edu/elsmancs/loremipsum/Main.java
@@ -6,22 +6,21 @@ public class Main {
     private static ContarCantidad contar;
 
     public static void main(String[] args) {
-        System.out
-                .println("###" + " Parrafos " + "###");
+        System.out.println("###" + " Frases " + "###");
+        System.out.println("Cantidad de parrafos: " + contar.contarFrases() + "\n");
+
+        System.out.println("###" + " Palabras " + "###");
+        System.out.println("Cantidad de parrafos: " + contar.contarPalabras() + "\n");
+
+        System.out.println("###" + " Parrafos " + "###");
         System.out.println("Cantidad de parrafos: " + contar.contarParrafos() + "\n");
 
-        System.out
-                .println("###" + " Palindromos " + "###");
-        System.out.println("Palabras palindromas: " + contar.contarPalindromos(contar.getLoremIpsum()));
+        System.out.println("###" + " Palindromos " + "###");
+        System.out.println("Palabras palindromas: " + contar.contarPalindromos(contar.listarPalabras(contar.getLoremIpsum())));
 
-        System.out.println("Cantidad: " + contar.contarPalindromos(contar.getLoremIpsum()).size() + "\n");
+        System.out.println("Cantidad: " + contar.contarPalindromos(contar.listarPalabras(contar.getLoremIpsum())).size() + "\n");
 
-        System.out
-                .println("###" + " Ordenar " + "###");
-        System.out.println(contar.listarPalabrasRepetidas(contar.getLoremIpsum()) + "\n");
-
-        System.out
-                .println("###" + " Ordenar mas repetidas " + "###");
-        System.out.println(contar.palabrasMasRepetidas(contar.listarPalabrasRepetidas(contar.getLoremIpsum())) + "\n");
+        System.out.println("###" + " Palabras mas repetidas " + "###");
+        System.out.println(contar.palabrasMasRepetidas(contar.listarPalabras(contar.getLoremIpsum())) + "\n");
     }
 }
