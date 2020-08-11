a3c1acdc5358b5a559b3c08b6b1515ae6bf33cae
javiBarrientos <javibarrientosfernandez99@gmail.com>
2020-08-11 16:19:58 +0200

    feat: Añadir main para ejecutar

--- src/main/java/edu/elsmancs/loremipsum/Main.java
+++ src/main/java/edu/elsmancs/loremipsum/Main.java
@@ -1,7 +1,27 @@
 package edu.elsmancs.loremipsum;
 
-public class Main {
-    public static void main(String[] args) {
+import edu.elsmancs.loremipsum.domain.ContarCantidad;
 
+public class Main {
+    private static ContarCantidad contar;
+
+    public static void main(String[] args) {
+        System.out
+                .println("###" + " Parrafos " + "###");
+        System.out.println("Cantidad de parrafos: " + contar.contarParrafos() + "\n");
+
+        System.out
+                .println("###" + " Palindromos " + "###");
+        System.out.println("Palabras palindromas: " + contar.contarPalindromos(contar.getLoremIpsum()));
+
+        System.out.println("Cantidad: " + contar.contarPalindromos(contar.getLoremIpsum()).size() + "\n");
+
+        System.out
+                .println("###" + " Ordenar " + "###");
+        System.out.println(contar.listarPalabrasRepetidas(contar.getLoremIpsum()) + "\n");
+
+        System.out
+                .println("###" + " Ordenar mas repetidas " + "###");
+        System.out.println(contar.palabrasMasRepetidas(contar.listarPalabrasRepetidas(contar.getLoremIpsum())) + "\n");
     }
 }
